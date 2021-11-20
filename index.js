76b577006af7d9b1e3891af72bd905798b4d5623
firasel <mdrasel601390@gmail.com>
2021-11-21 00:28:27 +0600

    test for deploy

--- index.js
+++ index.js
@@ -9,12 +9,8 @@ app.use(cors());
 app.use(cookieParser());
 
 const Test = require("./controller/User/Test");
-const Login = require("./controller/User/Login");
-const Register = require("./controller/User/Register");
-const Update = require("./controller/User/Update");
-const LoginGuard = require("./middleware/LoginGuard");
 const userHandler = require("./routeHandler/userHandler.js");
-// const taskHandler = require("./routeHandlers/taskHandler.js");
+const taskHandler = require("./routeHandler/taskHandler.js");
 
 const port = process.env.PORT || 3000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lwdhb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -31,7 +27,7 @@ mongoose
 app.use("/user", userHandler);
 
 // // Routes
-// app.use("/task", taskHandler);
+app.use("/task", taskHandler);
 
 app.use('/test',Test);
 
