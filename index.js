940c1fd21aa112d737a9d29f454d5e4238a1ba2d
firasel <mdrasel601390@gmail.com>
2021-11-21 00:03:47 +0600

    test for deploy

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+const userHandler = require("./routeHandlers/userHandler.js");
+const taskHandler = require("./routeHandlers//taskHandler.js");
+const SendResponse = require("./controller/SendResponse/SendResponse.js");
+
 const port = process.env.PORT || 3000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lwdhb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
@@ -19,11 +23,11 @@ mongoose
   .then(() => console.log("connection successful"))
   .catch((err) => console.log(err));
 
-// // Routes
-// app.use("/user", userHandler);
+// Routes
+app.use("/user", userHandler);
 
-// // Routes
-// app.use("/task", taskHandler);
+// Routes
+app.use("/task", taskHandler);
 
 app.get("/", (req, res) => {
   res.send("Api is worikng");
